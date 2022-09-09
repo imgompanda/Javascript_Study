2079e263571ada798890aeda13091d5a4f3956d1
gompanda <browoo051@Gmail.com>
2022-09-09 20:25:00 +0900

    자바스크립트 기초 및 Variable, Const

---
+++ Momentum/app.js
@@ -0,0 +1,11 @@
+// 바뀌지 않는 값을 나타낼 때
+const a = 5;
+const b = 2;
+
+const myName = 'hyungwoo';
+const veryLongVariableName = 'likeThis';
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log('hello' + myName);
